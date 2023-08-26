25b64294947c8b308be15110ec1c388f3090e104
adamreaksmey <adaminiature@gmail.com>
2023-08-26 18:33:23 +0700

    moved function

--- __main__.py
+++ __main__.py
@@ -1,41 +1,5 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-from concurrent.futures import ThreadPoolExecutor
+from all_downloader.main import downloadAllComposersVersions
 
-
-def download_file(url, path):
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-    with open(path, 'wb') as fd:
-        for chunk in response.iter_content(chunk_size=8192):
-            fd.write(chunk)
-
-
-def initDownloadComposersVersions():
-    folder_path = os.path.join(os.getcwd(), 'composers')
-    os.makedirs(folder_path, exist_ok=True)
-
-    url = 'https://getcomposer.org/download/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    links = soup.find_all('a')
-    composer_links = [urljoin(url, link['href']) for link in links if 'composer.phar' in link.get(
-        'href') and link.get('href').endswith('composer.phar') and 'latest' not in link.get('href')]
-
-    # Download and save the composer versions
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        for link in composer_links:
-            version = link.rsplit('/', 2)[-2]
-            composer_folder = os.path.join(
-                folder_path, 'dev' if version == "getcomposer.org" else version)
-            os.makedirs(composer_folder, exist_ok=True)
-
-            file_path = os.path.join(composer_folder, 'composer.phar')
-            executor.submit(download_file, link, file_path)
-            print(f"Downloaded: {file_path}")
-
-
+print("hello world, please dont run this yet, it will download every composer versions")
 if __name__ == "__main__":
-    initDownloadComposersVersions()
+    downloadAllComposersVersions()
