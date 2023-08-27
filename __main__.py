629acc80824ea6f7ac3a8f7af3cc8b6739cdbd73
adamreaksmey <adaminiature@gmail.com>
2023-08-27 12:04:06 +0700

    update

--- __main__.py
+++ __main__.py
@@ -1,8 +1,21 @@
 from downloader.main import initComposerDownload
 
 if __name__ == "__main__":
-    __input = input('Enter composer version to download: ')
-    if (__input == "all"):
-        initComposerDownload('')
+    question = """
+What would you like to do?:
+Please input a number:
+1. Download a single composer version
+2. Download all composer versions
+3. Switch composer version
+"""
+    print(question)
+    selection = input("Please choose a number: ")
+    if (selection == "1"):
+        seeAll = input("Would you like to see all composer versions? (Y/N)")
+        if (seeAll == "Y"):
+            initComposerDownload('see all')
+        else:
+            versionNumb = input("Please input a version to download: ")
+            initComposerDownload(versionNumb)
     else:
-        initComposerDownload(__input)
+        print("test dev")
