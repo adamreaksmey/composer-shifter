8775e46fc03f3ed7c939cf9e94c7bd8e9cc6e9f7
adamreaksmey <adaminiature@gmail.com>
2023-08-27 16:56:46 +0700

    update check install version

--- __main__.py
+++ __main__.py
@@ -1,26 +1,23 @@
-from downloader.main import initComposerDownload
+from downloader.__main__ import initComposerDownload
+from switcher.__main__ import getComposerVersions
 import os
 
 def download_single():
     see_all = input("Would you like to see all available composer versions? (Y/N) ")
     if see_all.upper() == "Y":
-        initComposerDownload('see all')
-    version_number = input(
-        "Please input a version to download (Example: 2.5.5, 2.5.6...etc): ")
+        initComposerDownload("see all")
+    version_number = input("Please input a version to download (Example: 2.5.5, 2.5.6...etc): ")
     initComposerDownload(version_number)
 
 
 def download_all():
     initComposerDownload('')
 
-
 def switch_version():
-    print("Functionality for switch version isn't implemented yet.")
-
+    return;
 
 def invalid_option():
     print("Invalid option chosen. Please enter a valid number.")
-
 
 options = {
     "1": download_single,
--- switcher/__main__.py
+++ switcher/__main__.py
@@ -1,19 +1,21 @@
 import os
 
-def getComposerContents():
-    # Get the path to the 'composers' folder
+def getComposerVersions():
     current_directory = os.path.abspath(os.curdir)
     parent_directory = os.path.dirname(current_directory)
     composers_folder = os.path.join(parent_directory, 'composers')
 
-    # Get all items (files and folders) within the 'composers' folder
+    if not os.path.exists(composers_folder):
+        print("You don't have any composer versions installed!")
+        return []
+
     composer_contents = []
     for item in os.listdir(composers_folder):
         item_path = os.path.join(composers_folder, item)
         composer_contents.append(os.path.basename(item_path))
     return composer_contents
 
-# Get and print the contents of the 'composers' folder
-contents = getComposerContents()
-for item in contents:
-    print(item)
+def checkInstalledVersion(version):
+    _allV = getComposerVersions()
+    if version in _allV:
+        print('test dev')
